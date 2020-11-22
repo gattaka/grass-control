eef35d568d6adf409faf37cd21db64524f497b85
Hynek Uhlíř <hynek.uhlir@gmail.com>
2020-11-22 22:04:38 +0100

    Sphinx4 upgrade; False-positives reduced; Grammar improved

--- src/main/java/cz/gattserver/grass/control/speech/SpeechControl.java
+++ src/main/java/cz/gattserver/grass/control/speech/SpeechControl.java
@@ -1,5 +1,7 @@
 package cz.gattserver.grass.control.speech;
 
+import java.io.IOException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,13 +9,20 @@ import cz.gattserver.grass.control.CmdControl;
 import cz.gattserver.grass.control.TrayControl;
 import cz.gattserver.grass.control.vlc.VLCCommand;
 import cz.gattserver.grass.control.vlc.VLCControl;
-import voce.SpeechInterface;
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.LiveSpeechRecognizer;
+import edu.cmu.sphinx.api.SpeechResult;
 
 public enum SpeechControl {
 
 	INSTANCE;
 
 	private static final Logger logger = LoggerFactory.getLogger(SpeechControl.class);
+
+	private static final String ACOUSTIC_MODEL = "resource:/edu/cmu/sphinx/models/en-us/en-us";
+	private static final String DICTIONARY_PATH = "resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict";
+	private static final String GRAMMAR_PATH = "resource:/gram/";
+	private static final String LANGUAGE_MODEL = "resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin";
 
 	private volatile boolean running = false;
 	private volatile boolean enabled = true;
@@ -27,7 +36,7 @@ public enum SpeechControl {
 			public void run() {
 				try {
 					runControl();
-				} catch (InterruptedException e) {
+				} catch (InterruptedException | IOException e) {
 					throw new IllegalStateException(SpeechControl.class.getName() + " was interrupted", e);
 				}
 			}
@@ -38,49 +47,62 @@ public enum SpeechControl {
 		running = false;
 	}
 
-	private void runControl() throws InterruptedException {
-		SpeechInterface.init();
+	private void runControl() throws InterruptedException, IOException {
+
+		Configuration configuration = new Configuration();
+		configuration.setAcousticModelPath(ACOUSTIC_MODEL);
+		configuration.setDictionaryPath(DICTIONARY_PATH);
+		configuration.setLanguageModelPath(LANGUAGE_MODEL);
+		configuration.setGrammarPath(GRAMMAR_PATH);
+		configuration.setUseGrammar(true);
+		configuration.setGrammarName("grammar");
+		// configuration.setSampleRate(48000);
+
+		LiveSpeechRecognizer recognizer = new LiveSpeechRecognizer(configuration);
+		SpeechResult result = null;
+		recognizer.startRecognition(true);
 		logger.info("Speech recognition initialized");
 		while (running) {
-			while (SpeechInterface.getRecognizerQueueSize() > 0) {
-				String s = SpeechInterface.popRecognizedString();
+			while ((result = recognizer.getResult()) != null) {
+				String s = result.getHypothesis();
+				if ("<unk>".equals(s))
+					continue;
+				// System.out.println(result.getResult().getFrameStatistics());
 				logger.info("You said: " + s);
 				switch (s) {
-				case "grass control open hardware":
-					break;
-				case "grass control open articles":
-					break;
-				case "grass control open calculator":
-					break;
-				case "grass control open you tube":
-					break;
-				case "v l c next":
+				case "v l c player next":
 					executeCommand(() -> {
 						VLCControl.INSTANCE.sendCommand(VLCCommand.NEXT);
 						TrayControl.INSTANCE.showMessage(s);
 					});
 					break;
-				case "v l c previous":
+				case "v l c player previous":
 					executeCommand(() -> {
 						VLCControl.INSTANCE.sendCommand(VLCCommand.NEXT);
 						TrayControl.INSTANCE.showMessage(s);
 					});
 					break;
-				case "v l c stop":
+				case "v l c player stop":
 					executeCommand(() -> {
 						VLCControl.INSTANCE.sendCommand(VLCCommand.PAUSE);
 						TrayControl.INSTANCE.showMessage(s);
 					});
 					break;
-				case "v l c play":
+				case "v l c player play":
 					executeCommand(() -> {
 						VLCControl.INSTANCE.sendCommand(VLCCommand.PLAY);
 						TrayControl.INSTANCE.showMessage(s);
 					});
 					break;
+				case "grass control open hardware":
+					executeCommand(() -> {
+						CmdControl.INSTANCE.openChrome("https://www.gattserver.cz/hw");
+						TrayControl.INSTANCE.showMessage(s);
+					});
+					break;
 				case "grass control open grass":
 					executeCommand(() -> {
-						CmdControl.INSTANCE.openChrome("www.gattserver.cz");
+						CmdControl.INSTANCE.openChrome("https://www.gattserver.cz");
 						TrayControl.INSTANCE.showMessage(s);
 					});
 					break;
@@ -93,7 +115,7 @@ public enum SpeechControl {
 				}
 			}
 		}
-		SpeechInterface.destroy();
+		recognizer.stopRecognition();
 	}
 
 	private void executeCommand(Command command) {
